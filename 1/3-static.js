eb6f6728bc6d1d07a9cbddd859d434563dcabffe
Anthony Ringoet <anthonyringoet@gmail.com>
2013-01-20 13:10:04 +0100

    fix static ex

--- 1/3-static.js
+++ 1/3-static.js
@@ -3,19 +3,37 @@
 
 var http = require('http');
 var path = require('path');
-// filesystem module ah yeah!
 var fs = require('fs');
+var mimeTypes = {
+  '.js' : 'text/javascript',
+  '.html' : 'text/html',
+  '.css' : 'text/css'
+};
 
 http.createServer(function (request, response){
   var lookup = path.basename(decodeURI(request.url)) || 'index.html',
     f = 'static/' + lookup;
 
   fs.exists(f, function(exists){
+    if(exists){
+      fs.readFile(f, function(err, data){
+        var headers = {'Content-type': mimeTypes[path. extname(lookup)]};
+        response.writeHead(200, headers);
+        response.end(data);
+
+        if(err){
+          response.writeHead(500);
+          response.end('Server error');
+          return;
+        }
+
+      });
+      return;
+    }
+    else{
+      response.writeHead(404);
+      response.end('404 - Item not found');
+    }
     console.log(exists ? lookup + ' is there' : lookup + ' doesn\'t exist');
   });
-
-  if(!response.finished){
-    response.writeHead(404);
-    response.end('Finishing this request in case anything goes wrong. \nSee log in console');
-  }
 }).listen(1337);
