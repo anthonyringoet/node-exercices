f5d5ef1b0ba480782792a9fbcbcd1c473b028819
Anthony Ringoet <anthonyringoet@gmail.com>
2013-03-16 14:34:15 +0100

    read csv

--- 4/1-csv.js
+++ 4/1-csv.js
@@ -1,9 +1,25 @@
 var csv = require('ya-csv');
+var options = {
+       'separator': '~',
+       'quote': '|',
+       'escape': '|'
+};
+var writer = csv.createCsvFileWriter('data.csv', options);
 
-var writer = csv.createCsvFileWriter('data.csv');
-
+// write to csv
 var data = [['pink floyd', 'marble sounds', 'kings of leon'], ['explosions in the sky', 'mogwai', 'godspeed you black emperor', 'codes in the clouds', 'mono']];
 
 data.forEach(function(rec){
   writer.writeRecord(rec);
+});
+
+
+// and now read it back from file
+var reader = csv.createCsvFileReader('data.csv', options);
+var data = [];
+
+reader.on('data', function(rec){
+  data.push(rec);
+}).on('end', function(){
+  console.log('here is your data back sir: \n\n' + data);
 });
